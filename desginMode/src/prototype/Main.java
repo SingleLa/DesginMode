f563f8810d6cdd92808c8ea8ca2dbb20f7f8c6eb
SingleLa <394950814@qq.com>
2018-04-14 00:21:14 +0800

    原生模式

--- desginMode/src/prototype/Main.java
+++ desginMode/src/prototype/Main.java
@@ -1,5 +1,11 @@
 package prototype;
 
+/**
+ * 原生模式：
+ * 我们在事先生成UnderlinePen  MessageBox 等product的实例  存储在Manager showcase map中
+ * 由于product 继承 Cloneable 接口
+ * 所以 后续的 p1 p2 p3 我们在这里没有 用new的方式生成 product的实力，而是通过Cloneable接口的clone方法
+ */
 public class Main {
     public static void main(String[] args){
      //准备
